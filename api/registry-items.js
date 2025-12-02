1332c7de4ac98758b1fcbb9f0cc82990ccdf53dc
underhillz <zach@zachunderhill.com>
2025-12-02 10:15:21 -0700

    Update and rename registry-items-TEST.js to registry-items.js

---
+++ api/registry-items.js
@@ -0,0 +1,14 @@
+export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return res.status(200).json({
+    message: "CORS test OK"
+  });
+}
